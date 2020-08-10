e463f6b57be843161935278b644daf9230f1d24a
Nadi11235 <nadi112355@gmail.com>
2020-08-10 18:17:50 +0300

    Add/components-and-databinding

---
+++ components-and-databinding/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  serverElements = [{
+    type: 'server',
+    name: 'TestServer',
+    content: 'Just a test!'
+  }];
+
+  onServerAdded(serverData) {
+      this.serverElements.push({
+        type: 'server',
+        name: serverData.name,
+        content: serverData.content
+      }); 
+    }
+  
+    onBlueprintAdded(bluePrintData)  {
+        this.serverElements.push({
+        type: 'blueprint',
+        name: bluePrintData.name,
+        content: bluePrintData.content
+      });
+    }
+}
