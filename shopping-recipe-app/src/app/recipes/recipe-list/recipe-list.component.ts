ac0bb2d5d852cfc5605abf273f867293ea7b6ae4
Nadi11235 <nadi112355@gmail.com>
2020-08-13 13:21:32 +0300

    add/recipe-detail-functionality-and-shopping-edit

---
+++ shopping-recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+  recipes: Recipe[] = [
+    new Recipe('A Test recipe', 'This is simply a test', 
+    'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg'),
+    new Recipe('Another test recipe', 'This is simply a test', 
+    'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg'),
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
+}
---
+++ shopping-recipe-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,24 @@
+import { Component, ViewChild, ElementRef, EventEmitter, Output } from '@angular/core'
+import { Ingredient } from 'src/app/shared/ingredient.model';
+
+@Component ({
+    selector: 'app-shopping-edit',
+    templateUrl: './shopping-edit.component.html',
+    styleUrls: ['./shopping-edit.component.css']
+})
+
+export class ShoppingListEditComponent {
+    
+    @ViewChild('nameInput') nameInputRef: ElementRef;
+    @ViewChild('amountInput') amountInputRef: ElementRef;
+    @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
+    constructor() {}
+
+    OnAddItem(){
+        const ingName = this.nameInputRef.nativeElement.value;
+        const ingAmount = this.amountInputRef.nativeElement.value;
+        const newIngredient = new Ingredient(ingName, ingAmount); 
+        this.ingredientAdded.emit(newIngredient);
+    }
+}
